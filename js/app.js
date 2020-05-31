2c75337fd06a01daaa1fe498f89cfffa05dda8e7
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-05-31 17:47:34 -0600

    frames and psuedo done, matrix established

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('Hello frontend');
+//global var establish for game board
+window.gameMatrix = [
+    [null, null, null]
+    [null, null, null]
+    [null, null, null]
+]; 
+//global var for player needed
+
+//checkers for win no loss possible on move
+
+function checkVert(e){
+    window.gameMatrix.forEach( => console.log(element));
+}
+
+function checkHori(){
+
+}
+
+
+funtion checkDiag(){
+
+}
+
+//board checker to init follow fn, log winner, send to message
+function checkBoard(player) {
+    let winner = false; 
+
+}
+
