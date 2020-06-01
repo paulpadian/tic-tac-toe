2276c93faff903c380b179ada35e869eaf9416a8
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-06-01 08:32:41 -0600

    working

--- js/app.js
+++ js/app.js
@@ -7,18 +7,16 @@ window.gameMatrix = [
 //global var for player needed
 
 window.player = "x";
-window.winner= undefined;
-
-
-//checkers for win no loss possible on move
+window.winner = undefined;
+//checkers for win no loss possible on move click
 
 function checkVert() {
-    
+
     window.gameMatrix[0].forEach((col, index) => {
         if (col === window.player) {
-            if(window.gameMatrix[1][index] === window.player && window.gameMatrix[2][index] === window.player) {
-            window.winner = window.player
-            displayWinner();                
+            if (window.gameMatrix[1][index] === window.player && window.gameMatrix[2][index] === window.player) {
+                window.winner = window.player
+                displayWinner();
             }
         }
     });
@@ -26,10 +24,10 @@ function checkVert() {
 };
 
 function checkHori() {
-  
+
     window.gameMatrix.forEach(row => {
         let count = 0;
-    
+
         row.forEach(block => {
             if (block === window.player) {
                 count++
@@ -45,62 +43,63 @@ function checkHori() {
 
 
 function checkDiag() {
-    if(window.gameMatrix[0][0] === window.player && window.gameMatrix[1][1] === window.player && window.gameMatrix[2][2] === window.player) {
-                window.winner = window.player
-                displayWinner();
+    if (window.gameMatrix[0][0] === window.player && window.gameMatrix[1][1] === window.player && window.gameMatrix[2][2] === window.player) {
+        window.winner = window.player
+        displayWinner();
     }
-    if(window.gameMatrix[0][2] === window.player && window.gameMatrix[1][1] === window.player && window.gameMatrix[2][0] === window.player) {
-                window.winner = window.player
-                displayWinner();
+    if (window.gameMatrix[0][2] === window.player && window.gameMatrix[1][1] === window.player && window.gameMatrix[2][0] === window.player) {
+        window.winner = window.player
+        displayWinner();
     }
 };
-    
 
 
-function checkBoard(){
-    if(winner) {
+
+function checkBoard() {
+    if (winner) {
         return displayWinner();
     }
-    if(!window.winner) {
+    if (!window.winner) {
         checkHori();
-    } 
-    if(!window.winner) {
+    }
+    if (!window.winner) {
         checkVert();
     }
-    if(!window.winner) {
+    if (!window.winner) {
         checkDiag();
     }
-  
+
 
 }
 
 function displayWinner() {
     console.log(`${window.winner} has won the game`)
     document.getElementById("display").innerText = `${window.player} has won the game`
-    
+
 }
 
 //board checker to init follow fn, log winner, send to message
-function checkForWinner() {
-    return checkBoard();
-}
 
 function updateBoard(position) {
     document.getElementById(position).innerText = window.player
     position = JSON.parse(position);
     window.gameMatrix[position[0]][position[1]] = window.player;
     console.log(window.gameMatrix)
-    checkForWinner();
-    if(window.player === "x") {
+    checkBoard();
+    if (window.player === "x") {
         window.player = "o"
-    } else [
+    } else[
         window.player = "x"
     ]
-    
+
 }
 
 document.querySelectorAll('.block').forEach(element => {
-    element.addEventListener('click', () => updateBoard(element.id));
+    element.addEventListener('click', () => {
+        if(!element.innerText) {
+            updateBoard(element.id)
+        }
+    });
 })
 
 
